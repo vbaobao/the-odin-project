1db0bb6716a5e435029af5432126c93827705567
vbaobao <vbao.main@gmail.com>
2019-08-31 16:37:07 -0700

    Create grid in js

--- web-development-101/etch-a-sketch/etchsketch.js
+++ web-development-101/etch-a-sketch/etchsketch.js
@@ -4,15 +4,16 @@ const width = 16;
 
 // Importing DOM elements
 const container = document.querySelector("#container");
-const canvas = document.createElement("div");
-
-// Adding inline style for the grid.
-canvas.classList.add("canvas");
-canvas.style.cssText = "background: gray; border-radius: 10px";
+container.style.cssText = "display: grid; grid-template-columns: repeat(16,auto); grid-template-rows: repeat(16,auto)"
 
 // Create grid using a nested for loop.
 for ( let x = 0; x < width; x++) {
     for ( let y = 0; y < height; y++) {
+        const canvas = document.createElement("div");
+        // Adding inline style for the grid.
+        canvas.classList.add("canvas");
+        canvas.style.cssText = "background: gray; border-radius: 10px";
+        canvas.textContent = "X";
         container.appendChild(canvas);
     }
 }

38ad1cf3d7c4e3babd135c6c01822ea4516f6c85
vbaobao <vbao.main@gmail.com>
2019-08-31 16:36:52 -0700

    Create grid in js

--- web-development-101/etch-a-sketch/etchsketch.js
+++ web-development-101/etch-a-sketch/etchsketch.js
@@ -1,1 +1,18 @@
+// Constants that define the canvas
+const height = 16;
+const width = 16;
 
+// Importing DOM elements
+const container = document.querySelector("#container");
+const canvas = document.createElement("div");
+
+// Adding inline style for the grid.
+canvas.classList.add("canvas");
+canvas.style.cssText = "background: gray; border-radius: 10px";
+
+// Create grid using a nested for loop.
+for ( let x = 0; x < width; x++) {
+    for ( let y = 0; y < height; y++) {
+        container.appendChild(canvas);
+    }
+}

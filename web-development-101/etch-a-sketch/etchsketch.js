73fa8e16e6171672e82818dfcb57951be7af3741
vbaobao <vbao.main@gmail.com>
2019-08-31 17:38:46 -0700

    Fix grids add container size

--- web-development-101/etch-a-sketch/etchsketch.js
+++ web-development-101/etch-a-sketch/etchsketch.js
@@ -1,10 +1,18 @@
-// Constants that define the canvas
-const height = 16;
-const width = 16;
+// Variables that define the canvas
+let height = 16;
+let width = 16;
+let containerSize = 960;
 
 // Importing DOM elements
 const container = document.querySelector("#container");
-container.style.cssText = "display: inline-grid; grid-template-columns: repeat(16, auto); grid-template-rows: repeat(16,auto)"
+container.style.cssText = "display: inline-grid; grid-template-columns: repeat("+ width +", auto); grid-template-rows: repeat("+ height +",auto); margin: auto; height "+ containerSize +"px; width: "+ containerSize +"px"
+
+const body = document.querySelector("body");
+const reset = document.createElement("button");
+body.style.cssText = "display: flex; flex-direction: column"
+reset.textContent = "RESET";
+reset.style.cssText = "background: #b4e6f5; border: 0; border-radius: 5px; margin: 25px auto";
+body.appendChild(reset);
 
 // Create grid using a nested for loop.
 for ( let x = 0; x < width; x++) {
@@ -12,11 +20,11 @@ for ( let x = 0; x < width; x++) {
         const canvas = document.createElement("div");
         // Adding inline style for the grid.
         canvas.classList.add("canvas");
-        canvas.style.cssText = "background: gray; border-radius: 5px; width: 35px; height: 35px";
+        canvas.style.cssText = "background: #373741; height: " + (containerSize/height) + "px";
         container.appendChild(canvas);
 
         canvas.addEventListener("mouseover", (e) => {
-            canvas.style.cssText = "background: yellow";
+            canvas.style.cssText = "background: #ffe0e0";
         });
     }
 }

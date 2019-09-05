37f5c48e25b10d101613d01b5bc75e8d02df2774
vbaobao <vbao.main@gmail.com>
2019-09-04 20:17:41 -0700

    Add operator functions

--- web-development-101/calculator/calculator.js
+++ web-development-101/calculator/calculator.js
@@ -1,11 +1,21 @@
-function add() {}
+function add(num1, num2) {
+    return num1 + num2;
+}
 
-function subtract() {}
+function subtract(num1, num2) {
+    return num1 - num2;
+}
 
-function multiply() {}
+function multiply(num1, num2) {
+    return num1 * num2;
+}
 
-function divide() {}
+function divide(num1, num2) {
+    return num1/num2;
+}
 
-function operate(operator, num1, num2) {}
+function operate(operator, num1, num2) {
+    
+}
 
 function showCalc() {}

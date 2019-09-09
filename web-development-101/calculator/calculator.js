1c5c0f37322bab03c678b312ac4084ad8190df2d
vbaobao <vbao.main@gmail.com>
2019-09-08 19:30:05 -0700

    Write equal function

--- web-development-101/calculator/calculator.js
+++ web-development-101/calculator/calculator.js
@@ -49,7 +49,7 @@ let display = document.querySelector("#display");
 
 // VARIABLES
 let operator;
-let num;
+let build = [];
 let text = "";
 
 // EVENT LISTENERS
@@ -57,15 +57,19 @@ for (let i = 0 ; i < operators.length ; i++ ) {
     operators[i].addEventListener("click", (e) => {
         if (i == 0) { 
             operator = "add";
+            build.push("add");
             text = "+"; }
         else if (i == 1) {
             operator = "substract";
+            build.push("subtract");
             text = "-"; }
         else if (i == 2) {
             operator = "multiply";
+            build.push("multiply");
             text = "*"; }
         else if (i == 3) {
             operator = "divide";
+            build.push("divide");
             text = "/";  }
     });
 
@@ -74,15 +78,80 @@ for (let i = 0 ; i < operators.length ; i++ ) {
 
 for (let i = 0 ; i < nums.length ; i++ ) {
     nums[i].addEventListener("click", (e) => {
-        num = i;
+        build.push(i);
     });
 
     update(display, text);
 }
 
 equal.addEventListener("click", (e) => {
-    text = "=";
-    update(display, text);
+    //EVERYTHING HAPPENS HERE, AFTER EQUAL IS CLICKED
+    //Checks if entry is valid. In number/string alternations
+    //Checks for order of operations
+    //Check division by 0
+    //Executes operations for the numbers before and after the op
+    let result = 0;
+    let array = build;
+
+    //Check if entries are valid
+    for (let i = 0; i < array.length; i++) {
+        if ( i == 0 || i%2 == 0 ) {
+            if ( typeof array[i] != "number") {
+                display.style.color = "red";
+                display.textContent = "Invalid entry";
+            }
+        }
+        else if ( i%2 != 0 ) {
+            if ( typeof array[i] != "string" ) {
+                display.style.color = "red";
+                display.textContent = "Invalid entry";
+            }
+        }
+    }
+    
+    //Checks for order of operation MDAS, left->right.
+    //Insert result of operation into the array.
+    //Remove elements of the array after it is calculated.
+    while (array.length > 1) {
+        for (let i = 0; i < array.length; i++) {
+            //multiply
+            if (array[i] == "multiply") {
+                result = operate(array[i], array[i-1], array[i+1]);
+                array = array.splice(i, 0, result);
+                array = array.splice(i-1, 3);
+            }
+        }
+        for (let i = 0; i < array.length; i++) {
+            //divide
+            if (array[i] == "divide") {
+                if ( array[i+1] == 0 ) {
+                    display.textContent = "Divide by 0 Error";
+                    array = [];
+                }
+                result = operate(array[i], array[i-1], array[i+1]);
+                array = array.splice(i, 0, result);
+                array = array.splice(i-1, 3);
+            }
+        }
+        for (let i = 0; i < array.length; i++) {
+            //add
+            if (array[i] == "add") {
+                result = operate(array[i], array[i-1], array[i+1]);
+                array = array.splice(i, 0, result);
+                array = array.splice(i-1, 3);
+            }
+        }
+        for (let i = 0; i < array.length; i++) {
+            //subtract
+            if (array[i] == "subtract") {
+                result = operate(array[i], array[i-1], array[i+1]);
+                array = array.splice(i, 0, result);
+                array = array.splice(i-1, 3);
+            }
+        }
+    }
+    console.log(array);
+    update(display, array[0]);
 });
 
 clear.addEventListener("click", (e) => {

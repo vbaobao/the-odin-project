9caa1f2be3a0fad200ea99cd619fc5578e2dfee8
vbaobao <vbao.main@gmail.com>
2019-09-08 18:02:22 -0700

    Add and modify update function

--- web-development-101/calculator/calculator.js
+++ web-development-101/calculator/calculator.js
@@ -15,44 +15,78 @@ function divide(num1, num2) {
     return num1/num2;
 }
 
-function operate(operator, num1, num2) {  
+function operate(operator, num1, num2) {
+    let result; 
+    if (operator == "add") {
+        result = add(num1,num2);
+    }
+    else if (operator == "subtract") {
+        result = subtract(num1,num2);
+    }
+    else if (operator == "multiply") {
+        result = multiply(num1,num2);
+    }
+    else if (operator == "divide") {
+        result = divide(num1,num2);
+    }
+    else (
+        console.log("error");
+    )
 }
 
 function reset() {}
 
-function showCalc() {}
+function update(display, text) {
+    display.textContent = display.textContent + " " + text;
+}
 
 // GET DOMS
 let operators = document.querySelectorAll(".operators");
 let nums = document.querySelectorAll(".nums");
 let equal = document.querySelector("#equal");
 let clear = document.querySelector("#clear");
+let display = document.querySelector("#display");
 
 // VARIABLES
 let operator;
-let num1;
-let num2;
+let num;
+let text = "";
 
 // EVENT LISTENERS
 for (let i = 0 ; i < operators.length ; i++ ) {
     operators[i].addEventListener("click", (e) => {
-        if (i == 0) { operator = "add"; }
-        else if (i == 1) { operator = "substract"; }
-        else if (i == 2) { operator = "multiply"; }
-        else if (i == 3) { operator = "divide"; }
+        if (i == 0) { 
+            operator = "add";
+            text = "+"; }
+        else if (i == 1) {
+            operator = "substract";
+            text = "-"; }
+        else if (i == 2) {
+            operator = "multiply";
+            text = "*"; }
+        else if (i == 3) {
+            operator = "divide";
+            text = "/";  }
     });
+
+    update(display, text);
 }
 
 for (let i = 0 ; i < nums.length ; i++ ) {
     nums[i].addEventListener("click", (e) => {
         num = i;
     });
+
+    update(display, text);
 }
 
 equal.addEventListener("click", (e) => {
-
+    text = "=";
+    update(display, text);
 });
 
 clear.addEventListener("click", (e) => {
-
+    text = "";
+    display.textContent = "";
+    update(display, text);
 });
